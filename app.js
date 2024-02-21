fc2b62e3271052d2b4b4dd07bee56913bdb2b28e
AlvitoDian <alvidpp23@gmail.com>
2024-02-21 20:31:47 +0700

    fix: route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
