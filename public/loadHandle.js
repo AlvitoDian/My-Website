4d7d03cb80cbe2281c5602a04d8ca2641ac8c29b
AlvitoDian <alvidpp23@gmail.com>
2024-03-25 18:49:56 +0700

    fix: loading

--- public/loadHandle.js
+++ public/loadHandle.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function () {
+/* window.addEventListener("load", function () {
   var loadingOverlay = document.getElementById("loading-overlay");
   var loadingText = document.getElementById("loading-text");
   const loadingBar = document.getElementById("myLoadingBar");
@@ -25,3 +25,46 @@ window.addEventListener("load", function () {
     }, 500);
   }, 500);
 });
+ */
+
+window.onload = function () {
+  var loadingOverlay = document.getElementById("loading-overlay");
+  var loadingText = document.getElementById("loading-text");
+  const loadingBar = document.getElementById("myLoadingBar");
+
+  const images = document.querySelectorAll("img");
+
+  const totalImages = images.length;
+
+  let imagesLoaded = 0;
+
+  function incrementImagesLoaded() {
+    imagesLoaded++;
+    var percentLoaded = Math.floor((imagesLoaded / totalImages) * 100);
+
+    loadingText.innerText = "Loading" + " (" + percentLoaded + "%)";
+    loadingBar.style.width = `${percentLoaded}%`;
+    loadingBar.style.setProperty("--loading-bar-width", percentLoaded + "%");
+
+    if (imagesLoaded === totalImages) {
+      loadingText.innerText = "Done!";
+      setTimeout(function () {
+        loadingOverlay.classList.add("loaded");
+      }, 1000);
+    }
+  }
+
+  images.forEach(function (img) {
+    if (img.complete) {
+      incrementImagesLoaded();
+    } else {
+      img.addEventListener("load", function () {
+        incrementImagesLoaded();
+      });
+    }
+  });
+
+  if (totalImages === 0) {
+    loadingText.innerText = "Tidak ada gambar yang ditemukan dalam dokumen.";
+  }
+};
