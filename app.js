22fdd2c8526f85239931d022456392de2f9f1393
AlvitoDian <alvidpp23@gmail.com>
2024-06-02 04:47:19 +0700

    feat: log

--- app.js
+++ app.js
@@ -4,7 +4,79 @@ const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
 const port = process.env.PORT || 3000;
+
+let logRecorded = false;
+
+//? MongoDB connection setup
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+}
+connectDB();
+//? MongoDB connection setup End
+
+//? Model LOG
+const logSchema = new mongoose.Schema({
+  message: String,
+  timestamp: Date,
+});
+
+const Log = mongoose.model("Log", logSchema);
+//? Model LOG End
+
+//? Morgan Logging
+const sendToMongoDB = async (message) => {
+  const logEntry = new Log({
+    message,
+    timestamp: new Date(),
+  });
+  await logEntry.save();
+};
+
+morgan.token("ip", (req) => {
+  const ip = req.headers["x-forwarded-for"] || req.ip;
+  return ip;
+});
+
+morgan.token("software", (req) => req.get("User-Agent") || "Unknown");
+
+morgan.format(
+  "custom",
+  ":ip :method :url :status :res[content-length] - :response-time ms - Software: :software"
+);
+
+//? Morgan Logging End
+
+//? Morgan Middleware
+const logFirstRequest = (req, res, next) => {
+  if (!logRecorded && req.method === "GET" && req.path === "/") {
+    morgan("custom", {
+      stream: {
+        write: (message) => {
+          sendToMongoDB(message);
+          console.log(message);
+          logRecorded = true;
+        },
+      },
+    })(req, res, next);
+  } else {
+    next();
+  }
+};
+
+app.use(logFirstRequest);
+//? Morgan Middleware End
 
 app.use(express.static("public"));
 
