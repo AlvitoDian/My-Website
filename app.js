b20c5ff6e039e8b2ff87aa25a673a6f6fb3a0e14
AlvitoDian <alvidpp23@gmail.com>
2024-06-02 18:29:51 +0700

    feat: log

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
-const morgan = require("morgan");
 const port = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 const mongodb = require("mongodb");
