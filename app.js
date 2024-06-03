b765387a7ad69c0103bd093a91e429a3137e1f08
AlvitoDian <alvidpp23@gmail.com>
2024-06-03 13:53:57 +0700

    feat: log

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const mongoose = require("mongoose");
 const mongodb = require("mongodb");
 const moment = require("moment");
 
-/* let logCalled = false;
+let logCalled = false;
 
 //? MongoDB connection setup
 async function connectDB() {
@@ -59,7 +59,7 @@ app.use((req, res, next) => {
   }
   next();
 });
-//? Log Express End */
+//? Log Express End
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
